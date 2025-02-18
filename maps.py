ea6f34d9e1ad937218c0ada72853919ce437a7bc
KingJaro27 <kingjaro27@gmail.com>
2025-02-18 18:59:52 +0300

    Add files via upload

--- maps.py
+++ maps.py
@@ -1,18 +1,17 @@
+import PIL.Image
 import requests
 import io
+import PIL
 
 
 def get_map(coord_x, coord_y, scale):
-    coord_x = coord_x
-    coord_y = coord_y
-    scale = scale
-
-    apikey = "f3a0fe3a-b07e-4840-a1da-06f18b2ddf13"
+    
+    apikey = "0fb296a1-6cd6-486e-bb8b-b680e29d3080" #f3a0fe3a-b07e-4840-a1da-06f18b2ddf13 
     map_params = {
         "ll": f"{coord_x},{coord_y}",
         "spn": f"{scale},{scale}",
         "l": "map",
-        "apikey": apikey,
+        #"apikey": apikey,
     }
 
     map_url = "https://static-maps.yandex.ru/1.x/"
@@ -20,4 +19,9 @@ def get_map(coord_x, coord_y, scale):
     resp.raise_for_status()
     image = io.BytesIO()
     image.write(resp.content)
+    #ii = PIL.Image.open(image)
+    #ii.show()
     return image
+
+
+get_map(37, 64, 5)
